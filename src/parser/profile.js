c1ee791c4fa04a336cbeb3d225236dc27306b9a9
kukuhbaiquni <kukuh.baiquni@gmail.com>
2022-10-29 21:56:18 +0700

    Add role instances

--- src/parser/profile.js
+++ src/parser/profile.js
@@ -1,5 +1,5 @@
 const parser_profile = ($) => {
-  const result = []
+  const result = {}
   $('.pi-item.pi-group.pi-border-color').each((_, el) => {
     const parrent = $(el).find('.pi-item.pi-data.pi-item-spacing.pi-border-color')
     parrent.find('.pi-data-value.pi-font').each((index, elm) => {
@@ -7,10 +7,8 @@ const parser_profile = ($) => {
        * Category
        * 4 Features
        * 6 Best in
-       * 13 Family
-       * 18 Alternative Name
        */
-      if (index === 4 || index === 6 || index === 13 || index === 18) {
+      if (index === 4 || index === 6) {
         const txt = [...$(elm).contents()]
           .filter((e) => $(e).text().trim()).map((e) => ({
             text: $(e).text().trim(),
@@ -18,6 +16,8 @@ const parser_profile = ($) => {
           }))
         result.push({ text: txt })
       } else {
+        if (index === 0) result.tier = $(elm).find('a').attr('href')
+        if (index === 1) result.type = $(elm).find('a').attr('href')
         result.push({
           text: $(elm).text(),
           url: $(elm).find('a').attr('href'),
--- src/utils/role-instances.js
+++ src/utils/role-instances.js
@@ -1,10 +1,19 @@
 const roleInstances = (url) => {
   switch (url) {
-  case 'https://static.wikia.nocookie.net/grandchase/images/c/cc/Rank-SS.png/revision/latest':
-    return 'Assault'
-
   case 'https://static.wikia.nocookie.net/grandchase/images/e/ea/Type-Mage.png/revision/latest/scale-to-width-down/24':
     return 'Mage'
+
+  case 'https://static.wikia.nocookie.net/grandchase/images/5/5a/Type-Ranged.png/revision/latest/scale-to-width-down/24':
+    return 'Ranger'
+
+  case 'https://static.wikia.nocookie.net/grandchase/images/5/5a/Type-Tank.png/revision/latest/scale-to-width-down/24':
+    return 'Ranger'
+
+  case 'https://static.wikia.nocookie.net/grandchase/images/5/5a/Type-Healer.png/revision/latest/scale-to-width-down/24':
+    return 'Ranger'
+
+  case 'https://static.wikia.nocookie.net/grandchase/images/5/5a/Type-Assault.png/revision/latest/scale-to-width-down/24':
+    return 'Ranger'
 
   default:
     return 'Unknown'
